2cfd27a37ae5b2632a615e8e15509f3c9fd867f7
Jouini abderraziqu <Interactive.Math.DZ@gmail.com>
2026-04-02 12:08:33 +0100

    Update verifyAndHydrate.js

--- assets/verifyAndHydrate.js
+++ assets/verifyAndHydrate.js
@@ -309,7 +309,7 @@ function finalizeExerciseState(exerciseID) {
 /**
  * دالة إعادة المحاولة: تصفير التمرين برمجياً وبصرياً
  */
-function resetPart(exerciseID) {
+function resetExercice(exerciseID){
     // 1. جلب كافة العناصر والمدخلات
     const allElements = document.querySelectorAll(`.${exerciseID}`);
     const btnVerify = document.getElementById(`${exerciseID}-btnVerify`);
